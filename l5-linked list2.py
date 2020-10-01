3011bc443f3f2ce262fae2675119ca76f513ab1b
Charankatragadda <70314741+Charankatragadda@users.noreply.github.com>
2020-10-01 16:15:41 +0530

    Create l5-linked list2.py

---
+++ l5-linked list2.py
@@ -0,0 +1,14 @@
+#To create a linked list with inputs from user console
+# creating an empty list 
+lst = [] 
+  
+# number of elements as input 
+n = int(input("Enter number of elements : ")) 
+  
+# iterating till the range 
+for i in range(0, n): 
+    ele = int(input()) 
+  
+    lst.append(ele) # adding the element
+print(lst)
+    
